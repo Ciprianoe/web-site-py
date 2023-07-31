db1117ac376470aa3178ee65f8d2f7ac3db701a6
ciprianoe <ciprianoescorche@gmail.com>
2023-07-30 22:18:28 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    title = 'Home Cipriano Escorche'
+    return render_template('home.html',active='home',title=title)
+
+@app.route('/about')
+def about():
+     title = 'About Cipriano Escorche'
+     return render_template('about.html', active='about',title=title)
+
+@app.route('/projects')
+def projects():
+     title = 'Projects Cipriano Escorche'
+     return render_template('projects.html',active='projects',title=title)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
